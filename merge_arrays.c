14219d64a456faf8eaf8afa63e41ebb04b5a0409
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-29 19:20:20 +0100

    Update merge_arrays.c

--- merge_arrays.c
+++ merge_arrays.c
@@ -2,40 +2,35 @@
 
 int main() {
     int taille1, taille2;
-    
-    // Lecture du premier tableau
+
+    // Lecture des données
     scanf("%d", &taille1);
-    int tab1[taille1];
+    int tableau1[taille1];
     for (int i = 0; i < taille1; i++) {
-        scanf("%d", &tab1[i]);
+        scanf("%d", &tableau1[i]);
     }
-    
-    // Lecture du deuxième tableau
+
     scanf("%d", &taille2);
-    int tab2[taille2];
+    int tableau2[taille2];
     for (int i = 0; i < taille2; i++) {
-        scanf("%d", &tab2[i]);
+        scanf("%d", &tableau2[i]);
     }
-    
+
     // Fusion des tableaux
-    int tab_fusion[taille1 + taille2];
-    
-    // Copie du premier tableau
+    int tableauFusionne[taille1 + taille2];
     for (int i = 0; i < taille1; i++) {
-        tab_fusion[i] = tab1[i];
+        tableauFusionne[i] = tableau1[i];
     }
-    
-    // Copie du deuxième tableau
     for (int i = 0; i < taille2; i++) {
-        tab_fusion[taille1 + i] = tab2[i];
+        tableauFusionne[taille1 + i] = tableau2[i];
     }
-    
+
     // Affichage du résultat
-    printf("Tableau fusionne : ");
+    printf("Tableau fusionné : ");
     for (int i = 0; i < taille1 + taille2; i++) {
-        printf("%d ", tab_fusion[i]);
+        printf("%d ", tableauFusionne[i]);
     }
     printf("\n");
-    
+
     return 0;
 }
