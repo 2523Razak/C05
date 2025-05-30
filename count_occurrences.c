639aa593496023684579acf4c3f26beb6edfdb35
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-30 11:24:46 +0100

    Update count_occurrences.c

--- count_occurrences.c
+++ count_occurrences.c
@@ -3,9 +3,6 @@
 int main() {
     int taille, element, compte = 0;
     
-    // Lire l'élément à rechercher
-    scanf("%d", &element);
-    
     // Lire la taille du tableau
     scanf("%d", &taille);
     
@@ -14,14 +11,19 @@ int main() {
     // Lire les éléments du tableau
     for (int i = 0; i < taille; i++) {
         scanf("%d", &tableau[i]);
-        
-        // Compter les occurrences au fur et à mesure
+    }
+    
+    // Lire l'élément à rechercher
+    scanf("%d", &element);
+    
+    // Compter les occurrences
+    for (int i = 0; i < taille; i++) {
         if (tableau[i] == element) {
             compte++;
         }
     }
     
-    // Afficher le résultat comme dans l'exemple
+    // Afficher le résultat comme dans l'exemple du PDF
     printf("L'element %d apparait %d fois.\n", element, compte);
     
     return 0;
